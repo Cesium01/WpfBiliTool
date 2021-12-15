869db1d82b68e88f47e648c3a399ba4106d0fe9e
Cesium <youthleaderzhj@gmail.com>
2021-12-16 01:26:02 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfBiliTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private FileStream fs;
+        private MyClient myClient;
+        public MainWindow()
+        {
+            InitializeComponent();
+            myClient = new MyClient();
+            filePathBox.Text = "（上传文件需要登录，尚未完成）";
+            uploadBtn.IsEnabled = false;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            if (fs != null)
+            {
+                fs.Flush();
+                fs.Close();
+                fs = null;
+            }
+        }
+
+        private void OpenFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog()
+            {
+                Filter = "(*.jpg;*.jpeg;*.png;*.bmp)|*.jpg;*.jpeg;*.png;*.bmp"
+            };
+            bool? result = dialog.ShowDialog();
+            if (result == true)
+            {
+                filePathBox.Text = dialog.FileName;
+                fs = (FileStream)dialog.OpenFile();
+            }
+        }
+
+        private async void UploadBtn_Click(object sender, RoutedEventArgs e)
+        {
+            uploadBtn.Content = "上传中...";
+            uploadBtn.IsEnabled = false;
+            if (fs!=null && fs.CanRead)
+            {
+                try
+                {
+                    longUrlBox.Text = await myClient.UploadImage(fs);
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("未选择文件或文件无法读取");
+            }
+            uploadBtn.Content = "上传图片";
+            uploadBtn.IsEnabled = true;
+        }
+
+        private async void GetShortUrlBtn_Click(object sender, RoutedEventArgs e)
+        {
+            getShortUrlBtn.Content = "获取中...";
+            getShortUrlBtn.IsEnabled = false;
+            if (!string.IsNullOrEmpty(longUrlBox.Text))
+            {
+                try
+                {
+                    shortUrlBox.Text = await myClient.GetShortUrl(longUrlBox.Text);
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("长链为空");
+            }
+            getShortUrlBtn.Content = "获取短链";
+            getShortUrlBtn.IsEnabled = true;
+        }
+
+        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            filePathBox.Text = "";
+            longUrlBox.Text = "";
+            shortUrlBox.Text = "";
+            topicNameBox.Text = "";
+            if (fs != null)
+            {
+                fs.Flush();
+                fs.Close();
+                fs = null;
+            }
+        }
+
+        private void TopicBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string encodedTopicName = System.Web.HttpUtility.UrlEncode(topicNameBox.Text, Encoding.UTF8).ToUpper();
+            System.Diagnostics.Process.Start("explorer.exe", "https://t.bilibili.com/topic/" + encodedTopicName);
+        }
+
+        private async void RollBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(rollNumberBox.Text) || string.IsNullOrEmpty(dynamicUrlBox.Text))
+            {
+                MessageBox.Show("抽奖人数或动态链接未填写");
+            }
+            else
+            {
+                Dictionary<string, string> users;
+                rollBtn.IsEnabled = false;
+                selectedBox.Text = "正在获取数据...";
+                try
+                {
+                    users = await myClient.DynamicRoll(dynamicUrlBox.Text, rollMethodBox.SelectedIndex);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    rollBtn.IsEnabled = true;
+                    selectedBox.Clear();
+                    return;
+                }
+                if (users.Count < int.Parse(rollNumberBox.Text))
+                {
+                    selectedBox.Clear();
+                    MessageBox.Show("回复/转发人数不足");
+                }
+                else
+                {
+                    Dictionary<string, string> selectedUsers = RollUsers(users, int.Parse(rollNumberBox.Text));
+                    selectedBox.Clear();
+                    foreach (string uid in selectedUsers.Keys)
+                    {
+                        selectedBox.Text += selectedUsers[uid] + ": https://space.bilibili.com/" + uid + "\n";
+                    }
+                }
+                rollBtn.IsEnabled = true;
+            }
+        }
+
+        private void RollNumber_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if((e.Key >= Key.D0 && e.Key <= Key.D9) || (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+                || e.Key == Key.Delete || e.Key == Key.Left || e.Key == Key.Right || e.Key == Key.Back)
+            {
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private static Dictionary<string, string> RollUsers(Dictionary<string, string> users, int number)
+        {
+            List<string> uidList = new List<string>(users.Keys);
+            HashSet<string> selectedSet = new HashSet<string>();
+            Random r = new Random();
+            while (selectedSet.Count < number)
+            {
+                selectedSet.Add(uidList[r.Next(users.Count)]);
+            }
+            Dictionary<string, string> selectedUsers = new Dictionary<string, string>();
+            foreach (string uid in selectedSet)
+            {
+                selectedUsers[uid] = users[uid];
+            }
+            return selectedUsers;
+        }
+    }
+}
---
+++ MyClient.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace WpfBiliTool
+{
+    internal class MyClient
+    {
+        public const string uploadApi = "https://api.bilibili.com/x/dynamic/feed/draw/upload_bfs";
+        public const string shortUrlApi = "http://api.bilibili.com/x/share/click";
+        public const string dynamicDetailApi = "https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/get_dynamic_detail?dynamic_id=";
+        public const string replyApi = "https://api.bilibili.com/x/v2/reply/main?jsonp=jsonp&mode=3&plat=1&type=";
+        public const string repostApi = "https://api.vc.bilibili.com/dynamic_repost/v1/dynamic_repost/repost_detail?dynamic_id=";
+        private static readonly string _s = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private static readonly CookieContainer cookieContainer = new CookieContainer();
+        private static readonly HttpClient client = new HttpClient(new HttpClientHandler()
+        {
+            AutomaticDecompression = DecompressionMethods.All,
+            CookieContainer = cookieContainer
+        });
+
+        public MyClient()
+        {
+            client.Timeout = TimeSpan.FromSeconds(18);
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Add("Accept", "*/*");
+            client.DefaultRequestHeaders.Add("DNT", "1");
+            client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip,deflate,br");
+            client.DefaultRequestHeaders.Add("Connection", "Keep-Alive");
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
+        }
+
+        ~MyClient()
+        {
+            client.Dispose();
+        }
+
+        public async Task<string> UploadImage(FileStream fs)
+        {
+            MultipartFormDataContent data = new MultipartFormDataContent();
+            string[] filePathSplit = fs.Name.Split('\\');
+            data.Add(new StreamContent(fs), "file_up", filePathSplit[filePathSplit.Length - 1]);
+            data.Add(new StringContent("dyn"), "biz");
+            data.Add(new StringContent("daily"), "category");
+            //TODO
+            data.Add(new StringContent(GetCookie("bili_jct")), "csrf");
+            HttpResponseMessage response = await client.PostAsync(uploadApi, data);
+            response.EnsureSuccessStatusCode();
+            JObject result = JObject.Parse(await response.Content.ReadAsStringAsync());
+            return ((int)result["code"] == 0) && (result["data"] as JObject).ContainsKey("content")
+                ? result["data"]["content"].ToString()
+                : throw new Exception(result.ToString());
+        }
+
+        public async Task<string> GetShortUrl(string longUrl)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>()
+            {
+                ["build"] = "6500300",
+                ["buvid"] = RandomBuvid(),
+                ["oid"] = longUrl,
+                ["platform"] = "android",
+                ["share_channel"] = "COPY",
+                ["share_id"] = "public.webview.0.0.pv",
+                ["share_mode"] = "3"
+            };
+            FormUrlEncodedContent data = new FormUrlEncodedContent(dic);
+            HttpResponseMessage response = await client.PostAsync(shortUrlApi, data);
+            response.EnsureSuccessStatusCode();
+            JObject result = JObject.Parse(await response.Content.ReadAsStringAsync());
+            return ((int)result["code"] == 0) && (result["data"] as JObject).ContainsKey("content")
+                ? result["data"]["content"].ToString()
+                : throw new Exception(result.ToString());
+        }
+
+        public async Task<Dictionary<string,string>> DynamicRoll(string dynamicUrl, int mode)
+        {
+            client.DefaultRequestHeaders.Add("Referer", dynamicUrl);
+            Dictionary<string, string> users = new Dictionary<string, string>();
+            Match match = Regex.Match(dynamicUrl, @"t\.bilibili\.com/(\d+)");
+            if(!match.Success)
+            {
+                throw new Exception("动态链接有误");
+            }
+            string dynamicId = match.Value.Replace("t.bilibili.com/", "");
+            string oid = dynamicId;
+            string type = "17";
+            JToken detailData = await FetchApi(dynamicDetailApi + dynamicId);
+            if ((int)detailData["card"]["desc"]["type"] == 2)
+            {
+                type = "11";
+                oid = (string)detailData["card"]["desc"]["rid"];
+            }
+            string fixedReplyApi = replyApi + type + "&oid=" + oid;
+            switch (mode)
+            {
+                case 0:
+                    await FetchReply(users, fixedReplyApi, "0");
+                    break;
+                case 1:
+                    await FetchRepost(users, dynamicId, "");
+                    break;
+                case 2:
+                    await FetchReply(users, fixedReplyApi, "0");
+                    await FetchRepost(users, dynamicId, "");
+                    break;
+                default:
+                    break;
+            }
+            client.DefaultRequestHeaders.Remove("Referer");
+            return users;
+        }
+
+        private async Task<JToken> FetchApi(string api)
+        {
+            var response = await client.GetAsync(api);
+            response.EnsureSuccessStatusCode();
+            JObject result = JObject.Parse(await response.Content.ReadAsStringAsync());
+            if ((int)result["code"] != 0)
+            {
+                throw new Exception(result.ToString());
+            }
+            else
+            {
+                return result["data"];
+            }
+        }
+
+        private async Task FetchReply(Dictionary<string, string> dic, string fixedApi, string _next)
+        {
+            long timeStamp = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            string api = fixedApi + "&next=" + _next + "&_=" + timeStamp;
+            JToken data = await FetchApi(api);
+            JToken cursor = data["cursor"];
+            JToken replies = data["replies"];
+            foreach(JToken reply in replies)
+            {
+                dic[reply["member"]["mid"].ToString()] = reply["member"]["uname"].ToString();
+            }
+            if (!(bool)cursor["is_end"])
+            {
+                await FetchReply(dic, fixedApi, (string)cursor["next"]);
+            }
+        }
+
+        private async Task FetchRepost(Dictionary<string, string> dic, string dynamicId, string offset)
+        {
+            string api = offset == "" ? repostApi + dynamicId : repostApi + dynamicId + "&offset=" + offset;
+            JToken data = await FetchApi(api);
+            JToken reposts = data["items"];
+            if (reposts != null)
+            {
+                foreach (JToken repost in reposts)
+                {
+                    dic[repost["desc"]["uid"].ToString()] = repost["desc"]["user_profile"]["info"]["uname"].ToString();
+                }
+            }
+            if ((int)data["has_more"] == 1)
+            {
+                await FetchRepost(dic, dynamicId, (string)data["offset"]);
+            }
+        }
+
+        public static string RandomBuvid()
+        {
+            StringBuilder sb = new StringBuilder();
+            Random rd = new Random();
+            for (int i = 0; i < 32; i++)
+            {
+                sb.Append(_s.Substring(rd.Next(62), 1));
+            }
+            return sb.ToString();
+        }
+
+        public string GetCookie(string name)
+        {
+            return cookieContainer.GetCookies(new Uri("https://www.bilibili.com/"))[name].Value;
+        }
+    }
+}
